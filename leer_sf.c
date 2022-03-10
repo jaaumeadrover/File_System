8bbd63d27066782f7fbe94ef16f37f989e2b845c
Joan Balaguer Llagostera <79452171+Joan13the13the13@users.noreply.github.com>
2022-03-10 11:39:41 +0100

    nivel 2 finished

--- leer_sf.c
+++ leer_sf.c
@@ -58,7 +58,7 @@ int main(int argc, char const *argv[])
 
         for (int j = 0; j < BLOCKSIZE / INODOSIZE; j++)
         {
-            if ((inodos[j].tipo == 'l'))
+            if (inodos[j].tipo == 'l')
             {
                 contlibres++;
                 if (contlibres < 20)
