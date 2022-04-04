8c3def2ad812ca6218f297db35dc96bba31db7f5
Joan Balaguer Llagostera <79452171+Joan13the13the13@users.noreply.github.com>
2022-04-04 07:22:11 +0200

    modificado makefile

--- permitir.c
+++ permitir.c
@@ -1,25 +1,29 @@
-#include "ficheros_basico.h"
+//#include "ficheros_basico.h"
+#include "ficheros.h"
 
 int main(int argc, char const *argv[]){
-    if (argc != 4)
-    {
+
+    //COmprobamos que la sintaxis sea correcta
+    if (argc != 4){
         fprintf(stderr, "Error sintaxis: ./permitir <nombre_dispositivo> <ninodo> <permisos>\n");
         return EXIT_FAILURE;
     }
 
     int ninodo = atoi(argv[2]);
-    int permisos = atoi(argv[2]);
-    char *camino = argv[1];
-
-    if (bmount(camino) == EXIT_FAILURE){
-        fprintf(stderr, "Error al montar el dispositivo.\n");
+    int permisos = atoi(argv[3]);
+    
+    if (bmount(argv[1]) == EXIT_FAILURE){
+        fprintf(stderr, "Error: al montar el dispositivo virtual en el método %s()",__func__);
         return EXIT_FAILURE;
     }
 
-    mi_chmod_f(ninodo,permisos); 
+    if (mi_chmod_f(ninodo,permisos)) {
+        fprintf(stderr, "Error: al modificar los permisos en el método %s()",__func__);
+        return EXIT_FAILURE;
+    }
 
     if (bumount() == EXIT_FAILURE){
-        fprintf(stderr, "Error al desmontar el dispositivo.\n");
+        fprintf(stderr, "Error: al desmontar el dispositivo virtual en el método %s()",__func__);
         return EXIT_FAILURE;
     }
 }
