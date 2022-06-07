1f7f871fdaba8b9cf754e0a4714b51da9b9b35e8
Jaume Adrover Fernández <69900925+jaaumeadrover@users.noreply.github.com>
2022-06-07 16:36:21 +0200

    Add files via upload

--- leer_sf.c
+++ leer_sf.c
@@ -1,9 +1,23 @@
-#include "ficheros_basico.h"
-//CONSTANTES PARA DEBUG DE CADA RESPECTIVO NIVEL
-#define DEBUGN1 0
-#define DEBUGN2 0
-#define DEBUGN3 0
-#define DEBUGN4 1
+//Autores: Eduardo Sánchez, Joan Balaguer, Jaume Adrover
+#include "directorios.h"
+
+
+
+
+//método debug nivel7
+void mostrar_buscar_entrada(char *camino, char reservar){
+  unsigned int p_inodo_dir = 0;
+  unsigned int p_inodo = 0;
+  unsigned int p_entrada = 0;
+  int error;
+  printf("\ncamino: %s, reservar: %d\n", camino, reservar);
+  if ((error = buscar_entrada(camino, &p_inodo_dir, &p_inodo, &p_entrada, reservar, 6)) < 0) {
+    mostrar_error_buscar_entrada(error);
+  }
+  printf("**********************************************************************\n");
+  return;
+}
+
 
 
 //La ejecución de leer_sf.c permite mostrar el contenido del superbloque.
@@ -45,11 +59,27 @@ int main(int argc, char const *argv[]){
     printf("totBloques = %d\n", SB.totBloques);
     printf("totInodos = %d\n", SB.totInodos);
 
+ #if DEBUGN7
+      //Mostrar creación directorios y errores
+  mostrar_buscar_entrada("pruebas/", 1); //ERROR_CAMINO_INCORRECTO
+  mostrar_buscar_entrada("/pruebas/", 0); //ERROR_NO_EXISTE_ENTRADA_CONSULTA
+  mostrar_buscar_entrada("/pruebas/docs/", 1); //ERROR_NO_EXISTE_DIRECTORIO_INTERMEDIO
+  mostrar_buscar_entrada("/pruebas/", 1); // creamos /pruebas/
+  mostrar_buscar_entrada("/pruebas/docs/", 1); //creamos /pruebas/docs/
+  mostrar_buscar_entrada("/pruebas/docs/doc1", 1); //creamos /pruebas/docs/doc1
+  mostrar_buscar_entrada("/pruebas/docs/doc1/doc11", 1);  
+  //ERROR_NO_SE_PUEDE_CREAR_ENTRADA_EN_UN_FICHERO
+  mostrar_buscar_entrada("/pruebas/", 1); //ERROR_ENTRADA_YA_EXISTENTE
+  mostrar_buscar_entrada("/pruebas/docs/doc1", 0); //consultamos /pruebas/docs/doc1
+  mostrar_buscar_entrada("/pruebas/docs/doc1", 1); //creamos /pruebas/docs/doc1
+  mostrar_buscar_entrada("/pruebas/casos/", 1); //creamos /pruebas/casos/
+  mostrar_buscar_entrada("/pruebas/docs/doc2", 1); //creamos /pruebas/docs/doc2
+
+#endif
     #if DEBUGN2
         printf("\nsizeof struct superbloque: %ld\n", sizeof(struct superbloque));
         printf("sizeof struct inodo:  %ld\n", sizeof(struct inodo));
 
-    
         printf("\nRECORRIDO LISTA ENLAZADA DE INODOS LIBRES\n\n");
         
         struct inodo inodos[BLOCKSIZE / INODOSIZE];
